7e96f2314197b8579713733209b0379dac7ba01f
raf_0707 <Raf_0707@mail.ru>
2022-12-02 21:00:05 +0300

    first lesson

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button saveAge;
+    private EditText editAge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        saveAge = findViewById(R.id.saveAge);
+        editAge = findViewById(R.id.editAge);
+
+        saveAge.setOnClickListener(View -> {
+            editAge.setCursorVisible(false);
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Возраст сохранен", Toast.LENGTH_SHORT);
+            toast.show();
+        });
+    }
+}
