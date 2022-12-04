17184ff40fe620342f4b95be4ab8e7ad773d9753
raf_0707 <Raf_0707@mail.ru>
2022-12-04 18:58:10 +0300

    Second Lesson new code

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.notes;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,50 +8,52 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button saveAge;
     private Button editAgeButton;
     private EditText editAge;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        saveAge = findViewById(R.id.saveAge);
         editAge = findViewById(R.id.editAge);
+        editAge.setOnClickListener(this);
 
-        saveAge.setOnClickListener(View -> {
-            editAge.setCursorVisible(false);
-
-            /*
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "Возраст сохранен", Toast.LENGTH_SHORT);
-            toast.show();
-
-             */
-
-        });
-
+        saveAge = findViewById(R.id.saveAgeButton);
+        saveAge.setOnClickListener(this);
         editAgeButton = findViewById(R.id.editAgeButton);
-        editAgeButton.setOnClickListener(View -> {
-            editAge.setCursorVisible(true);
-            /*
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "измените возраст", Toast.LENGTH_SHORT);
-            toast.show();
-
-             */
-
-        });
+        editAgeButton.setOnClickListener(this);
 
     }
 
     public void saveAge(View view) {
-        Snackbar.make(view, "Возраст сохранен", Snackbar.LENGTH_LONG).show();
-        //.setAction("Изменить", v -> {
-
-        //}).show();
+        editAge.setCursorVisible(false);
+        Snackbar.make(view, "Возраст сохранен", Snackbar.LENGTH_SHORT)
+        .setAction("Изменить", v -> {
+            editAge.setCursorVisible(true);
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "измените возраст", Toast.LENGTH_SHORT);
+            toast.show();
+        }).show();
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.saveAgeButton:
+                saveAge(view);
+                break;
+            case R.id.editAgeButton:
+                editAge.setCursorVisible(true);
+                break;
+            case R.id.editAge:
+                editAge.setCursorVisible(true);
+                break;
+        }
+    }
+
 }
